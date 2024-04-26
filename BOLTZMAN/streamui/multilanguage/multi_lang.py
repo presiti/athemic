07e4e15bdc03a8967c232ebbd81688fd14ccc8b4
AzureAD\변윤진 <zinna0347@datacentric.kr>
2024-04-26 15:16:26 +0900

    boltzman 다국어

--- BOLTZMAN/streamui/multilanguage/multi_lang.py
+++ BOLTZMAN/streamui/multilanguage/multi_lang.py
@@ -1,28 +1,80 @@
 item_caption={
+'model_list':{'kor':'모델 목록','eng':'Model List'},
+'modle_test_file':{'kor':'모델 테스트 파일','eng':'Model Test File'},
+'modle_select':{'kor':'모델 선택','eng':'Select Model'},
+
+'drop_ds_error':{'kor':'파일 제거 오류 발생','eng':'A file removal error occurred'},
+
+'data_preview_file':{'kor':'파일 미리보기','eng':'Preview the file'},
+'data_unable_process':{'kor':'처리 할 수 없는 데이터 입니다.','eng':'Data that cannot be processed.'},
+
 'data_manage':{'kor':'데이터 관리','eng':'Data Manage'},
 'file_upload':{'kor':'파일 등록','eng':'File Upload'},
 'listup_ds':{'kor':'데이터 목록 보기','eng':'Listup Dataset'},
-'select_algorithm':{'kor':'알고리즘 선택','eng':'Select Algorithm'},
-'data_preview':{'kor':'데이터 미리보기','eng':'Preview Dataset'},
-'data_preview_preprocess':{'kor':'전처리 데이터 미리보기','eng':'Preview Preprocess Dataset'},
-'name_model':{'kor':'모델 이름 설정','eng':'Name of Model'},
-'parameter':{'kor':'매개변수 조정','eng':'Parameter'},
-'preprocess_preview':{'kor':'전처리 미리보기','eng':'Preview Preprocess'},
-'rename_ds':{'kor':'데이터셋 이름 변경','eng':'Renaming Dataset'},
-'show_pca':{'kor':'주성분 분석','eng':'Show PCA'},
-'test_predict':{'kor':'모델 테스트','eng':'Predict test data'},
-'detected_missing_data':{'kor':'누락 데이터 발견','eng':'Detected Missing Data'},
+'delete_ds_butoon':{'kor':'{} 삭제','eng':'{} Delete'},
+'delete_ds_request':{'kor':'파일을 삭제하면 복구할 수 없습니다. 그래도 삭제하려면 파일명 {}을 기입하세요'
+                    ,'eng':'If you delete a file, you can\'t recover it. If you still want to delete it, enter the filename {}'},
+'delete_ds_name':{'kor':'파일명','eng':'File Name'},
+'delete_ds':{'kor':'파일 삭제','eng':'Delete this File'},
+
 'drop_col':{'kor':'추출할 특성<컬럼>', 'eng':'Target Feature<Column>'},
-'upload_prep_ds':{'kor':'전처리 데이터 저장', 'eng': 'Save to Storage'},
-'do_analysis':{'kor':'분석 요청','eng':'Do analysis'},
-'call_report':{'kor':'분석 결과 리포팅','eng':'Report analysis result'},
+'preprocess_preview':{'kor':'전처리 미리보기','eng':'Preview Preprocess'},
+
+'select_algorithm':{'kor':'알고리즘 선택','eng':'Select Algorithm'},
+
+'statistics':{'kor':'통계 옵션','eng':'Statistics Options'},
+
 'msg_sel_classification_algorithm':{'kor':'분류 알고리즘 선택','eng':'Select Classification algorithms'},
 'msg_sel_regression_algorithm':{'kor':'회귀 알고리즘 선택','eng':'Select Regression algorithms'},
 'msg_sel_neworreview':{'kor':'신규 모델 / 기존 모델 리뷰','eng':'New model / Review History'},
 
+'name_model':{'kor':'모델 이름 설정','eng':'Name of Model'},
+'do_analysis':{'kor':'분석 요청','eng':'Do analysis'},
+'call_report':{'kor':'분석 결과 리포팅','eng':'Report analysis result'},
+
+'process_not_runnig':{'kor':'프로세스가 작동 중이 아닙니다.','eng':'The process is not running.'},
+'process_finish':{'Session {}이 처리 완료 되었습니다.':'','eng':'Session {} has been processed.'},
+'process_error':{'kor':'Session {}의 처리 중 오류가 발생하였습니다.',
+                 'eng':'An error occurred during processing of Session {}.'},
+'parameter':{'kor':'매개변수 조정','eng':'Parameter'},
+
+'data_preview':{'kor':'데이터 미리보기','eng':'Preview Dataset'},
+'data_preview_warrning':{'kor':'데이터 미리 보기에서는 데이터를 수정할 수 없습니다.',
+                         'eng':'You can\'t modify data in the data preview.'},
+'data_name':{'kor':'데이터 이름','eng':'Data Name'},
+'data_description':{'kor':'데이터 설명','eng':'Data Description'},
+'data_preview_preprocess':{'kor':'전처리 데이터 미리보기','eng':'Preview Preprocess Dataset'},
+'data_correction_preprocess':{'kor':'전처리 데이터 미리 보기에서는 데이터를 셀에서도 수정할 수 있습니다.',
+                              'eng':'In the preprocessed data preview, you can also modify the data in the cells.'},
+'data_extract_attributes':{'kor':'특성(필드)추출','eng':'Extract Attributes(Fields)'},
+'data_change_type':{'kor':'데이터 타입 변경','eng':'Changing Data Types'},
+'data_change_value':{'kor':'값 변경','eng':'Change Values'},
+'data_add_column_head':{'kor':'컬럼 헤더 추가','eng':'Add Column Headers'},
+
+'data_tpye_string':{'kor':'문자형','eng':'String'},
+'data_tpye_int':{'kor':'정수형','eng':'Int'},
+'data_tpye_float':{'kor':'실수형','eng':'Float'},
+'rename_ds':{'kor':'데이터셋 이름 변경','eng':'Renaming Dataset'},
+'upload_prep_ds':{'kor':'전처리 데이터 저장', 'eng': 'Save to Storage'},
+
+'show_pca':{'kor':'주성분 분석','eng':'Show PCA'},
+'nominal_value_er_msg': {'kor':'숫자가 아닌 값을 포함하는 특성 컬럼 발견되었습니다.','eng':'Found Not a number field in "Input Features"'},
+'pca_missing_col':{'kor':'주성분 분석 데이터에 클래스 컬럼이 누락되었습니다.',
+                   'eng':'Principal Component Analysis data is missing a class column.'},
+'pca_preprocess_col':{'kor':'전처리 대상 컬럼 {}','eng':'column to Preprocessing target  {}'},
+'pca_class_col':{'kor':'클래스 컬럼','eng':'Class Columns {}'},
+'pca_attribute_columns':{'kor':'특성 컬럼','eng':'Attribute Columns'},
+
+'remove_model': {'kor':'결과 모델 삭제','eng':'Remove Model'},
+'test_predict':{'kor':'모델 테스트','eng':'Predict test data'},
+'session_id': {'kor':'모델 세션 ID','eng':'Session Id'},
+'test_model': {'kor':'결과 모델 테스트','eng':'Model Test'},
+'session_name': {'kor':'모델 세션 명','eng':'Session Name'},
+
+'detected_missing_data':{'kor':'누락 데이터 발견','eng':'Detected Missing Data'},
+
 'features_field':{'kor':'컬럼(특성) 선택','eng':'Features'},
 'class_field':{'kor':'클래스','eng':'Class field'},
-'sel_opt':{'kor':'포함 / 제거','eng':'Inc/Exc'},
 
 'rfc_numtrees':{'kor':'트리 개수','eng':'Numbers of Tree'},
 'rfc_maxdepth':{'kor':'트리 깊이','eng':'Max Tree depth'},
@@ -46,6 +98,24 @@ item_caption={
 'rfr_maxleaf_nodes':{'kor':'최대 리프 노드수','eng':'Max Leaf Nodes'},
 'rfr_min_impurity_decrease':{'kor':'최소 불순도 감소','eng':'Min impurity decrease'},
 
+'sel_opt':{'kor':'포함 / 제거','eng':'Inc/Exc'},
+'sel_opt_include':{'kor':'선택 컬럼 포함','eng':'Include Selection Columns'},
+'sel_opt_exclude':{'kor':'선택 컬럼 제거','eng':'Exclude selection columns'},
+
+'model_check_class_field':{'kor':'클래스 필드를 선택하세요','eng':'Select the Class field'},
+'model_check_class_one':{'kor':'1 개의 클래스만 존재합니다. 다중 분류 알고리즘을 수행할 수 없습니다.'
+                ,'eng':'Only 1 class exists. Cannot perform multiple classification algorithms.'},
+'model_check_attribute_null':{'kor':'특성 필드가 비어 있습니다.','eng':'The Attribute field is empty.'},
+'model_check_class_null':{'kor':'클래스 필드가 비어 있습니다.','eng':'The Class field is empty.'},
+'model_check_max_features':{'kor':'트리의 최대 특성 개수는 입력 특성의 개수를 초과할 수 없습니다.',
+                        'eng':'The maximum number of attributes in the tree cannot exceed the number of input attributes.'},
+
+'rfr_check_class_not_number':{'kor':'{}에서 숫자가 아닌 값을 특성 값을 발견하였습니다.',
+                                'eng':'We found a non-numeric value in {} as an attribute value.'},
+
+'insert_header_basic_name':{'kor':'기본 컬럼 이름','eng':'Default Column Name'},
+'insert_header_apply':{'kor':'컬럼 이름 적용','eng':'Apply Column Name'},
+
 
 'angle_label': {'kor':'X축 기울임 정도','eng':'Label Angle'},
 
@@ -61,10 +131,5 @@ item_caption={
 'origin_value': {'kor':'원본 값','eng':'Origin value'},
 'rep_value': {'kor':'교체할 값','eng':'Replace value'},
 'row_range': {'kor':'행 범위 선택','eng':'Select row range'},
-'nominal_value_er_msg': {'kor':'숫자가 아닌 값을 포함하는 특성 컬럼 발견됨','eng':'Found Not a number field in "Input Features"'},
-'remove_model': {'kor':'결과 모델 삭제','eng':'Remove Model'},
-'test_model': {'kor':'결과 모델 테스트','eng':'Model Test'},
-'session_id': {'kor':'모델 세션 ID','eng':'Session Id'},
-'session_name': {'kor':'모델 세션 명','eng':'Session Name'},
 'upoload_preprocess_ds': {'kor':'전처리 데이터 업로드','eng':'Upload Preprocess Data'}
  }
